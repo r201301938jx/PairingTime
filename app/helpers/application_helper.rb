38fccb2cb9859605910b8ad16f38fc2eed1fa25e
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-12 11:20:22 +0900

    Update tab title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def full_title(title = "")
+    base_title = "PairingTime"
+    if admin_signed_in?
+      base_title + " | " + "[管理者] #{title}"
+    else
+      base_title + " | " + "#{title}"
+    end
+  end
+
   def full_name(customer)
     "#{customer.last_name} #{customer.first_name}"
   end
