6593f25f3e347e81c5fb13d70192b44ee6a2660f
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-13 13:17:28 +0900

    Update notification feature

--- app/helpers/customer/notifications_helper.rb
+++ app/helpers/customer/notifications_helper.rb
@@ -17,4 +17,8 @@ module Customer::NotificationsHelper
     end
   end
 
+  def unchecked_notifications
+    @notifications = current_customer.passive_notifications.where(checked: false)
+  end
+
 end
