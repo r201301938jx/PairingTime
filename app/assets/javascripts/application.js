2a450c094c6bd231fc4df7ae9c2e71610f578c1e
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-16 16:17:45 +0900

    Add page-top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,25 @@ $(document).ready(function () {
     hidePrevious : false
   });
 });
+
+/* トップに戻る */
+
+$(function() {
+  $("#back-to-top").hide();
+
+  $(window).scroll(function(){
+    $('#pos').text($(this).scrollTop());
+    if ($(this).scrollTop() > 60){
+      $("#back-to-top").fadeIn();
+    }else{
+      $("#back-to-top").fadeOut();
+    }
+  })
+
+  $('#back-to-top a').click(function() {
+    $('html, body').animate({
+      scrollTop:0
+    }, 800);
+    return false;
+  })
+})
