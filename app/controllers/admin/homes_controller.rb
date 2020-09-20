85f5e04c5ecd09fd89b99b2918c6bf9207107806
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-20 13:05:05 +0900

    Fix chart cords for deploy environment

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -6,10 +6,10 @@ class Admin::HomesController < ApplicationController
     @today_pairs = Pair.where(created_at: Time.zone.now.all_day)
     @today_likes = Like.where(created_at: Time.zone.now.all_day)
     @today_comments = Comment.where(created_at: Time.zone.now.all_day)
-    @customer_data = Customer.group("date(created_at, '+00:00', '+09:00')").count
-    @pair_data = Pair.group("date(created_at, '+00:00', '+09:00')").count
-    @like_data = Like.group("date(created_at, '+00:00', '+09:00')").count
-    @comment_data = Comment.group("date(created_at, '+00:00', '+09:00')").count
+    @customer_data = Customer.group("date(created_at)").count
+    @pair_data = Pair.group("date(created_at)").count
+    @like_data = Like.group("date(created_at)").count
+    @comment_data = Comment.group("date(created_at)").count
     @tag_data = Tagging.eager_load(:tag).group(:'tags.name').count
   end
 end
