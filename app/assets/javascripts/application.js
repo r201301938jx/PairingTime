fdf3a029ce91edbf57d7553a0ecc0411a8f468f0
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-20 14:40:57 +0900

    Add tab menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -70,6 +70,8 @@ document.addEventListener("turbolinks:load", function() {
     })
   })
 
+  /* タブメニュー */
+
   $('#tab-contents .tab[id != "tab1"]').hide();
 
   $('#tab-menu a').on('click', function(event) {
