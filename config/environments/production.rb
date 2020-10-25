a488c777b19b77970606918fd35f376e4cbe9068
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-10-25 17:13:58 +0900

    Update email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     port: 587,
     address: 'smtp.gmail.com',
-    domain: 'smtp.gmail.com',
+    domain: 'gmail.com',
     user_name: ENV['EMAIL_ADDRESS'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: 'plain',
