a4130aac4a6bd30043ec6b2d2366b8b63afdb4ba
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-10-25 17:10:41 +0900

    Update email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
     user_name: ENV['EMAIL_ADDRESS'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: 'plain',
-    enable_starttls_auto: true
+    # enable_starttls_auto: true
   }
 end
