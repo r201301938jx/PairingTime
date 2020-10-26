243a4494874c5e0646c512d621f7a72a8fc37f3b
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-10-26 20:05:37 +0900

    Update email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #パスワード再発行
-  config.action_mailer.default_url_options = {host: 'http://pairingtime.work'}
+  config.action_mailer.default_url_options = {host: 'pairingtime.work'}
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
