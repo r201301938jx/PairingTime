92506131724bd36eebb0d8c2f4ff327562908a2c
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-10-26 20:33:26 +0900

    Update email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
   #パスワード再発行
   config.action_mailer.default_url_options = {host: 'pairingtime.work'}
 
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
